db79e5e7f5ee4d10fec0fc2dd4813bc16f80c721
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-11 13:20:23 +0200

    started challenge3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Challenge3 from "./WhatAreProps/Challenge3";
 // import Challenge1 from "./MapAndFilter/Challenge1";
-import Challenge2 from "./WhatIsState/Challenge2";
+// import Challenge2 from "./WhatIsState/Challenge2";
 
 function App() {
   return (
     <div className="App">
       {/* <Challenge1 /> */}
-      <Challenge2 />
+      {/* <Challenge2 /> */}
+      <Challenge3 />
     </div>
   );
 }
