b5d463a4189f7cb330a9e01d8ce63ae1f1c43bcb
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-20 11:00:55 +0200

    added challenge5

--- src/EventHandling/Challenge5.jsx
+++ src/EventHandling/Challenge5.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Challenge5() {
-  return <div>Challenge5</div>;
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  const getInputNum1 = (e) => {
+    setNum1(Number(e.target.value));
+  };
+
+  const getInputNum2 = (e) => {
+    setNum2(Number(e.target.value));
+  };
+
+  const addNumbers = (num1, num2) => {
+    console.log(num1 + num2);
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="Add numbers..."
+        onChange={getInputNum1}
+        name="input1"
+      />
+      <input
+        placeholder="Add numbers..."
+        onChange={getInputNum2}
+        name="input2"
+      />
+
+      <button onClick={() => addNumbers(num1, num2)}>Add Numbers</button>
+    </div>
+  );
 }
 
 export default Challenge5;
