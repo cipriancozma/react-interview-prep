9308d03f4b72096a03901defaeeb349061d0573d
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-29 17:33:01 +0200

    added challenge9 and started the 10th one

--- src/CssInReact/Challenge9.jsx
+++ src/CssInReact/Challenge9.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
+
+const styles = {
+  internal: {
+    color: "blue",
+  },
+  dark: {
+    backgroundColor: "white",
+    color: "black",
+  },
+  light: {
+    backgroundColor: "black",
+    color: "white",
+  },
+};
 
 function Challenge9() {
-  return <div>Challenge9</div>;
+  const [theme, setTheme] = useState(false);
+
+  const toogleTheme = () => {
+    setTheme(!theme);
+  };
+
+  return (
+    <div style={theme ? styles.light : styles.dark}>
+      <h1 className="external">Hello from Extern</h1>
+
+      <h1 style={styles.internal}>Hello from Intern</h1>
+
+      <h1 style={{ border: "1px solid green", color: "green" }}>
+        Hello from inline
+      </h1>
+
+      <button onClick={toogleTheme}>Toogle Theme</button>
+    </div>
+  );
 }
 
 export default Challenge9;
---
+++ src/UncontrolledVsControlled/Challenge10.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Challenge10() {
+  return <div>Challenge10</div>;
+}
+
+export default Challenge10;
