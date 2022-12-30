95abae1473e7a9f8ffd89b99f38412e30de90103
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-30 15:56:26 +0200

    added virtual dom

--- src/VirtualDOM/Challenge11.jsx
+++ src/VirtualDOM/Challenge11.jsx
@@ -3,7 +3,15 @@ import React from "react";
 function Challenge11() {
   return (
     <div>
-      <p>What is Virtual DOM?</p>
+      <h2>What is Virtual DOM?</h2>
+      <p>
+        Virtual DOM is a programming concept where a representation of the UI is
+        kept in memory and synced with the "real" DOM
+      </p>
+      <p>
+        In the process, only the updated nodes will be changed, nothing else.
+        This process is known as Reconciliation
+      </p>
     </div>
   );
 }
