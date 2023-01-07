c015b20d33fe96e8cd403f94660fcd1ff541f820
Ciprian <cozma_ciprian19@yahoo.com>
2023-01-07 17:48:32 +0200

    added the diff between class vs classNames

--- src/ClassVsClassnames/Challenge18.jsx
+++ src/ClassVsClassnames/Challenge18.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Challenge18() {
-  return <div>Challenge18</div>;
+// className -> used to add style to a div (equivalent of class from html)
+// class -> used to create classes in React
+// if we used class for styling instead className it will work but it shows a warning in the console (Invalid DOM property)
+class Challenge18 extends React.Component {
+  render() {
+    return <div className="header">Challenge18</div>;
+  }
 }
 
 export default Challenge18;
